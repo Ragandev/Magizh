f18d09e6abeed94bb131ea239a90c368f95da0e4
Ragandev <ranjith44.hhh@gmail.com>
2023-07-24 11:37:57 +0530

    Product List Page Responsive Done

--- js/product_list.js
+++
@@ -1,8 +0,0 @@
-// var priceSlider = document.querySelector(`input[type = range]`).value;
-// var priceValue = document.querySelector(`.value`);
-
-
-// function price(){
-//     priceValue.innerHTML = priceSlider;
-// }
-// setInterval(price, 300)
